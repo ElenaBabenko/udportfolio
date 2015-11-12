d46991041c5305cd21fee33c76a5929599a92036
Elena Babenko <ebabenko@constantcontact.com>
2015-11-12 16:30:27 -0500

    more optimizations

--- README.md
+++ README.md
@@ -1,6 +1,40 @@
-## Website Performance Optimization portfolio project
+# Website Performance Optimization portfolio project
 
-Here a the optimizations I performed.
+## How to get started
+
+1. Check out my repository: https://github.com/ElenaBabenko/udportfolio
+1. To inspect the site on your phone, you can run a local server
+
+  ```bash
+  $> cd /path/to/your-project-folder
+  $> python -m SimpleHTTPServer 8080
+  ```
+
+1. Open a browser and visit localhost:8080
+1. Download and install [ngrok](https://ngrok.com/) to make your local server accessible remotely.
+
+  ``` bash
+  $> cd /path/to/your-project-folder
+  $> ngrok 8080
+  ```
+
+1. Copy the public URL ngrok gives you and try running it through PageSpeed Insights! Optional: [More on integrating ngrok, Grunt and PageSpeed.](http://www.jamescryer.com/2014/06/12/grunt-pagespeed-and-ngrok-locally-testing/)
+
+## How to test
+
+### Testing index.html
+
+2. Copy the URL from ngrok and add index.html to the end of it.
+2. Go to [PageSpeed Insights](https://developers.google.com/speed/pagespeed/insights/) and enter the URL in the field.
+2. Click Analyze and review the results.
+
+### Testing pizza.html
+
+3. To test pizza.html copy the URL from ngrok and add /views/pizza.html to the end of it.
+2. Open the page in Chrome browser.
+3. Go to View > Developer > Developer tools to test.
+
+## Optimizations I performed
 
 ### For index.html
 
@@ -15,22 +49,24 @@ Didn't have time to figure out what to do with analytics JS and minification too
 
 ### For pizza.html
 
+#### Overall:
+
 - Optimized pizzaria.jpeg from over 2 MB to 200 KB
+- Minified CSS
+- Moved all static width and height styling from JS to CSS files
+- Replaced repeated selectors with a variables where possible
+- Cached variables where possible
 
 #### For the moving pizzas:
 
-- Reduced number of background pizzas from 200 to smaller size based on screen size
-- replaced document.querySelectorAll with document.getElementsByClassName
-- in updatePositions() replaced basicLeft with translate3D, moved scrollTop out of the loop
-- in mark-up made movingPizzas1 div span the whole width of the container so that the moving pizzas always appear in the same place horizontally. Initially their position would jump between screen sizes.
+- Replaced document.querySelectorAll with document.getElementsByClassName where possible
+- Moved layout calculations outside of the loop
+- In updatePositions() replaced basicLeft with translate3D
+- Added will-change: transform; and backface-visibility: hidden; to .mover to optimize brower rendering
+
 
 #### For the pizza size switcher:
 
-- reduced number of ingredient and name options, as well as the number of generated pizzas
-- replaced a repeated selector with a variable (pizzaElems)
-- moved layout calculations outside of the loop
-- added translateZ to .randomPizzaContainer img to force in on a separate layer
-- switched how pizza images sizes are updated. Instead of changing column width I used transform scale. Not sure why I'm not seeing improvement in performance? and why the class is not being added to the first two pizza?
-- tried adding transform: translateZ(0) and will-change to .randomPizzaContainer img but it seemed to make performance even worse, generated a lot of goast bars...
-
-
+- Switched how pizza images sizes are updated. Instead of changing each column width on the fly I just assign a different class (eg, small-size) to the parent container and control width of the child elements thru css coming from the stylesheet.
+- Added will-change: width; to .randomPizzaContainer to optimize brower rendering
+- in CSS added media queries and changed column sizes to better control how pizza images appear on small screens. Before, if you go to screen less than ~600px and select small size the images won't have enough space to render and won't show up at all.
--- views/js/main.js
+++ views/js/main.js
@@ -1,12 +1,12 @@
 /*
-Welcome to the 60fps project! Your goal is to make Cam"s Pizzeria website run
+Welcome to the 60fps project! Your goal is to make Cam's Pizzeria website run
 jank-free at 60 frames per second.
 
 There are two major issues in this code that lead to sub-60fps performance. Can
 you spot and fix both?
 
 
-Built into the code, you"ll find a few instances of the User Timing API
+Built into the code, you'll find a few instances of the User Timing API
 (window.performance), which will be console.log()ing frame rate data into the
 browser console. To learn more about User Timing API, check out:
 http://www.html5rocks.com/en/tutorials/webperformance/usertiming/
@@ -29,61 +29,87 @@ pizzaIngredients.meats = [
   "Chorizo",
   "Chicken Andouille",
   "Salami",
+  "Tofu",
   "Bacon",
   "Canadian Bacon",
   "Proscuitto",
   "Italian Sausage",
   "Ground Beef",
+  "Anchovies",
   "Turkey",
   "Ham",
   "Venison",
   "Lamb",
   "Duck",
+  "Soylent Green",
+  "Carne Asada",
   "Soppressata Picante",
   "Coppa",
   "Pancetta",
+  "Bresola",
+  "Lox",
   "Guanciale",
+  "Chili",
   "Beef Jerky",
   "Pastrami",
-  "Kielbasa"
+  "Kielbasa",
+  "Scallops",
+  "Filet Mignon"
 ];
 pizzaIngredients.nonMeats = [
   "White Onions",
   "Red Onions",
   "Sauteed Onions",
-  "Caramelized Onions",
   "Green Peppers",
   "Red Peppers",
   "Banana Peppers",
+  "Ghost Peppers",
+  "Habanero Peppers",
   "Jalapeno Peppers",
+  "Stuffed Peppers",
   "Spinach",
   "Tomatoes",
   "Pineapple",
+  "Pear Slices",
+  "Apple Slices",
   "Mushrooms",
   "Arugula",
   "Basil",
   "Fennel",
   "Rosemary",
   "Cilantro",
+  "Avocado",
+  "Guacamole",
+  "Salsa",
+  "Swiss Chard",
   "Kale",
   "Sun Dried Tomatoes",
+  "Walnuts",
   "Artichoke",
   "Asparagus",
+  "Caramelized Onions",
+  "Mango",
   "Garlic",
   "Olives",
   "Cauliflower",
+  "Polenta",
   "Fried Egg",
-  "Zucchini"
+  "Zucchini",
+  "Hummus"
 ];
 pizzaIngredients.cheeses = [
   "American Cheese",
   "Swiss Cheese",
+  "Goat Cheese",
   "Mozzarella Cheese",
   "Parmesean Cheese",
+  "Velveeta Cheese",
   "Gouda Cheese",
   "Muenster Cheese",
+  "Applewood Cheese",
   "Asiago Cheese",
   "Bleu Cheese",
+  "Boursin Cheese",
   "Brie Cheese",
   "Cheddar Cheese",
   "Chevre Cheese",
@@ -93,6 +119,7 @@ pizzaIngredients.cheeses = [
   "Gruyere Cheese",
   "Limberger Cheese",
   "Manchego Cheese",
+  "Marscapone Cheese",
   "Pecorino Cheese",
   "Provolone Cheese",
   "Queso Cheese",
@@ -124,12 +151,28 @@ String.prototype.capitalize = function() {
 // Pulls adjective out of array using random number sent from generator
 function getAdj(x){
   switch(x) {
+    case "dark":
+      var dark = ["dark","morbid", "scary", "spooky", "gothic", "deviant", "creepy", "sadistic", "black", "dangerous", "dejected", "haunted",
+      "morose", "tragic", "shattered", "broken", "sad", "melancholy", "somber", "dark", "gloomy", "homicidal", "murderous", "shady", "misty",
+      "dusky", "ghostly", "shadowy", "demented", "cursed", "insane", "possessed", "grotesque", "obsessed"];
+      return dark;
+    case "color":
+      var colors = ["blue", "green", "purple", "grey", "scarlet", "NeonGreen", "NeonBlue", "NeonPink", "HotPink", "pink", "black", "red",
+      "maroon", "silver", "golden", "yellow", "orange", "mustard", "plum", "violet", "cerulean", "brown", "lavender", "violet", "magenta",
+      "chestnut", "rosy", "copper", "crimson", "teal", "indigo", "navy", "azure", "periwinkle", "brassy", "verdigris", "veridian", "tan",
+      "raspberry", "beige", "sandy", "ElectricBlue", "white", "champagne", "coral", "cyan"];
+      return colors;
     case "whimsical":
       var whimsy = ["whimsical", "silly", "drunken", "goofy", "funny", "weird", "strange", "odd", "playful", "clever", "boastful", "breakdancing",
       "hilarious", "conceited", "happy", "comical", "curious", "peculiar", "quaint", "quirky", "fancy", "wayward", "fickle", "yawning", "sleepy",
       "cockeyed", "dizzy", "dancing", "absurd", "laughing", "hairy", "smiling", "perplexed", "baffled", "cockamamie", "vulgar", "hoodwinked",
       "brainwashed"];
       return whimsy;
+    case "shiny":
+      var shiny = ["sapphire", "opal", "silver", "gold", "platinum", "ruby", "emerald", "topaz", "diamond", "amethyst", "turquoise",
+      "starlit", "moonlit", "bronze", "metal", "jade", "amber", "garnet", "obsidian", "onyx", "pearl", "copper", "sunlit", "brass", "brassy",
+      "metallic"];
+      return shiny;
     case "noisy":
       var noisy = ["untuned", "loud", "soft", "shrieking", "melodious", "musical", "operatic", "symphonic", "dancing", "lyrical", "harmonic",
       "orchestral", "noisy", "dissonant", "rhythmic", "hissing", "singing", "crooning", "shouting", "screaming", "wailing", "crying", "howling",
@@ -145,7 +188,9 @@ function getAdj(x){
       var insulting = ["stupid", "idiotic", "fat", "ugly", "hideous", "grotesque", "dull", "dumb", "lazy", "sluggish", "brainless", "slow",
       "gullible", "obtuse", "dense", "dim", "dazed", "ridiculous", "witless", "daft", "crazy", "vapid", "inane", "mundane", "hollow", "vacuous",
       "boring", "insipid", "tedious", "monotonous", "weird", "bizarre", "backward", "moronic", "ignorant", "scatterbrained", "forgetful", "careless",
-      "lethargic", "insolent", "indolent", "loitering"];
+      "lethargic", "insolent", "indolent", "loitering", "gross", "disgusting", "bland", "horrid", "unseemly", "revolting", "homely", "deformed",
+      "disfigured", "offensive", "cowardly", "weak", "villainous", "fearful", "monstrous", "unattractive", "unpleasant", "nasty", "beastly", "snide",
+      "horrible", "syncophantic", "unhelpful", "bootlicking"];
       return insulting;
     case "praise":
       var praise = ["beautiful", "intelligent", "smart", "genius", "ingenious", "gorgeous", "pretty", "witty", "angelic", "handsome", "graceful",
@@ -157,7 +202,7 @@ function getAdj(x){
     case "scientific":
       var scientific = ["scientific", "technical", "digital", "programming", "calculating", "formulating", "cyberpunk", "mechanical", "technological",
       "innovative", "brainy", "chemical", "quantum", "astro", "space", "theoretical", "atomic", "electronic", "gaseous", "investigative", "solar",
-      "extinct", "galactic"]
+      "extinct", "galactic"];
       return scientific;
     default:
       var scientific = ["scientific", "technical", "digital", "programming", "calculating", "formulating", "cyberpunk", "mechanical", "technological",
@@ -174,7 +219,10 @@ function getNoun(y) {
       var animals = ["flamingo", "hedgehog", "owl", "elephant", "pussycat", "alligator", "dachsund", "poodle", "beagle", "crocodile", "kangaroo",
       "wallaby", "woodpecker", "eagle", "falcon", "canary", "parrot", "parakeet", "hamster", "gerbil", "squirrel", "rat", "dove", "toucan",
       "raccoon", "vulture", "peacock", "goldfish", "rook", "koala", "skunk", "goat", "rooster", "fox", "porcupine", "llama", "grasshopper",
-      "gorilla", "monkey", "seahorse", "wombat", "wolf", "giraffe", "badger", "lion", "bat", "mongoose", "swan", "firefly", "platypus"];
+      "gorilla", "monkey", "seahorse", "wombat", "wolf", "giraffe", "badger", "lion", "mouse", "beetle", "cricket", "nightingale",
+      "hawk", "trout", "squid", "octopus", "sloth", "snail", "locust", "baboon", "lemur", "meerkat", "oyster", "frog", "toad", "jellyfish",
+      "butterfly", "caterpillar", "tiger", "hyena", "zebra", "snail", "pig", "weasel", "donkey", "penguin", "crane", "buzzard", "vulture",
+      "rhino", "hippopotamus", "dolphin", "sparrow", "beaver", "moose", "minnow", "otter", "bat", "mongoose", "swan", "firefly", "platypus"];
       return animals;
     case "profession":
       var professions = ["doctor", "lawyer", "ninja", "writer", "samurai", "surgeon", "clerk", "artist", "actor", "engineer", "mechanic",
@@ -204,6 +252,16 @@ function getNoun(y) {
       "centipede", "beetle", "fart", "snot", "crevice", "flatulence", "juice", "mold", "mildew", "germs", "discharge", "toilet", "udder", "odor", "substance",
       "fluid", "moisture", "garbage", "trash", "bug"];
       return gross;
+    case "everyday":
+      var everyday = ["mirror", "knife", "fork", "spork", "spoon", "tupperware", "minivan", "suburb", "lamp", "desk", "stereo", "television", "TV",
+      "book", "car", "truck", "soda", "door", "video", "game", "computer", "calender", "tree", "plant", "flower", "chimney", "attic", "kitchen",
+      "garden", "school", "wallet", "bottle"];
+      return everyday;
+    case "jewelry":
+      var jewelry = ["earrings", "ring", "necklace", "pendant", "choker", "brooch", "bracelet", "cameo", "charm", "bauble", "trinket", "jewelry",
+      "anklet", "bangle", "locket", "finery", "crown", "tiara", "blingBling", "chain", "rosary", "jewel", "gemstone", "beads", "armband", "pin",
+      "costume", "ornament", "treasure"];
+      return jewelry;
     case "places":
       var places = ["swamp", "graveyard", "cemetery", "park", "building", "house", "river", "ocean", "sea", "field", "forest", "woods", "neighborhood",
       "city", "town", "suburb", "country", "meadow", "cliffs", "lake", "stream", "creek", "school", "college", "university", "library", "bakery",
@@ -224,8 +282,8 @@ function getNoun(y) {
   };
 };
 
-var adjectives = ["whimsical", "noise", "apocalyptic", "insulting", "praise", "scientific"];  // types of adjectives for pizza titles
-var nouns = ["animals", "fantasy", "gross", "horror", "places", "scifi"];                        // types of nouns for pizza titles
+var adjectives = ["dark", "color", "whimsical", "shiny", "noise", "apocalyptic", "insulting", "praise", "scientific"];  // types of adjectives for pizza titles
+var nouns = ["animals", "everyday", "fantasy", "gross", "horror", "jewelry", "places", "scifi"];                        // types of nouns for pizza titles
 
 // Generates random numbers for getAdj and getNoun functions and returns a new pizza name
 function generator(adj, noun) {
@@ -314,21 +372,22 @@ var pizzaElementGenerator = function(i) {
   pizzaImage = document.createElement("img");
   pizzaDescriptionContainer = document.createElement("div");
 
+  // adding a class that controls pizza resizing on the parent element
+  pizzaResize = document.querySelector("#randomPizzas");
+
   pizzaContainer.classList.add("randomPizzaContainer");
-  pizzaContainer.style.width = "33.33%";
-  pizzaContainer.style.height = "325px";
-  pizzaContainer.id = "pizza" + i;                // gives each pizza element a unique id
-  pizzaImageContainer.classList.add("col-md-6");
+  // pizzaContainer.style.width = "33.33%";         // removing to add it thru stylesheet
+  // pizzaContainer.style.height = "325px";         // removing to add it thru stylesheet
+  pizzaContainer.id = "pizza" + i;                  // gives each pizza element a unique id
+  pizzaImageContainer.classList.add("col-xs-5");    // changed column size for better visual layout
 
   pizzaImage.src = "images/pizza.png";
-  pizzaImage.classList.add("pizza-image");
   pizzaImage.classList.add("img-responsive");
-
   pizzaImageContainer.appendChild(pizzaImage);
   pizzaContainer.appendChild(pizzaImageContainer);
 
 
-  pizzaDescriptionContainer.classList.add("col-md-6");
+  pizzaDescriptionContainer.classList.add("col-xs-7"); // changed column size for better visual layout
 
   pizzaName = document.createElement("h4");
   pizzaName.innerHTML = randomName();
@@ -348,6 +407,7 @@ var resizePizzas = function(size) {
 
   // Changes the value for the size of the pizza above the slider
   function changeSliderLabel(size) {
+    // creating a variable to cache the element
     var sizeLabel = document.getElementById("pizzaSize");
 
     switch(size) {
@@ -367,29 +427,36 @@ var resizePizzas = function(size) {
 
   changeSliderLabel(size);
 
-  // Iterates through pizza elements on the page and changes their widths
-  // Cameron"s solution from the class, I think I don"t need to improve on this
+  // Setting style class on a parent element to resize randomPizzaContainers
   function changePizzaSizes(size) {
-    var newWidth;
+    var resizeDiv, widthClassName, arrayWidthClasses, i;
+
+    // variable that stores the array of all class names for sizing pizzas
+    arrayWidthClasses = ["small-size","medium-size","large-size"];
+    //variable to cache location of the target element
+    resizeDiv = document.querySelector("#randomPizzas");
 
     switch(size) {
       case "1":
-        newWidth = 0.5;
+        widthClassName = arrayWidthClasses[0]; // assigning a class name to a variable that will pass it on into the for loop below
         break;
       case "2":
-        newWidth = 1;
+        widthClassName = arrayWidthClasses[1];
         break;
       case "3":
-        newWidth = 1.5;
+        widthClassName = arrayWidthClasses[2];
         break;
       default:
         console.log("bug in sizeSwitcher");
     }
 
-    var pizzaElems = document.getElementsByClassName("pizza-image");
-
-    for (var i = 0; i < pizzaElems.length; i++) {
-      pizzaElems[i].style.transform = "scale(" + newWidth + ")";
+    // iterating thru all the class names in the array to see if it's already present in the element's class property
+    // if it is, remove it before adding the new size (widthClassName) that is set by the slider
+    for (i = 0; i < arrayWidthClasses.length; i++) {
+      if (resizeDiv.classList.contains(arrayWidthClasses[i]) === true) {
+        resizeDiv.classList.remove(arrayWidthClasses[i]);
+        resizeDiv.classList.add(widthClassName);
+      }
     }
   }
 
@@ -405,7 +472,7 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
-for (var i = 2; i < 30; i++) {
+for (var i = 2; i < 100; i++) {
   var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
@@ -430,27 +497,34 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
   console.log("Average time to generate last 10 frames: " + sum / 10 + "ms");
 }
 
-// The following code for sliding background pizzas was pulled from Ilya"s demo found at:
+// The following code for sliding background pizzas was pulled from Ilya's demo found at:
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
+  // caching all new variables needed in the function
   var scrollSpeed, constArray, numPizzas, phase, items, i;
 
   frame++;
   window.performance.mark("mark_start_frame");
 
+  // Speed of the scroll on the screen
   scrollSpeed = (document.body.scrollTop / 1250);
-
+  // caching the array of the pizza images that we will apply transform to
   items = document.getElementsByClassName("mover");
+  // caching number of images to transform
   numPizzas = items.length;
 
+  // iterating over the pizza images and appling transformation to each one
   for (i = 0; i < numPizzas; i++) {
+    // phase is calculating distance values that cause pizzas to move sideways on scroll
     phase = Math.sin(scrollSpeed + (i % 5));
-    items[i].style.transform = "translate3D(" + (500 * phase) + "px, 0, 0)";
+    // using transform style instead of left to decrease painting time
+    // 100 seems to be a "magic number" that controls to what degree each pizza image moves to the left and right
+    items[i].style.transform = "translate3D(" + (items[i].basicLeft + 100 * phase) + "px, 0, 0)";
   }
 
-  // User Timing API to the rescue again. Seriously, it"s worth learning.
+  // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
   window.performance.mark("mark_end_frame");
   window.performance.measure("measure_frame_duration", "mark_start_frame", "mark_end_frame");
@@ -465,30 +539,34 @@ window.addEventListener("scroll", updatePositions);
 
 // Generates the moving pizzas when the page loads.
 document.addEventListener("DOMContentLoaded", function() {
-  var vertSpace, horSpace, screenHeight, screenWidth, cols, rows, numMovingPizzas;
+  var colSpace, rowSpace, screenHeight, screenWidth, cols, rows, numMovingPizzas;
 
-      /* Tried window.innerHeight and width but it doesn"t refresh number of pizzas on window resize.
-         So using screen.availableHeight and width to make sure that there is enough pizzas
-         for the largest window size possible */
+      // Using screen.availableHeight and width to make sure that there is enough pizzas for the largest window size possible
       screenHeight = screen.availHeight;
       screenWidth = screen.availWidth;
 
-      // setting initial spacing between pizza images
-      rows = 5;
-      horSpace = 300;
-      vertSpace = Math.round(screenHeight/rows);
-      cols = Math.round(screenWidth/horSpace);
+      // setting and calculating initial spacing between pizza images
+      cols = 8;
+      rowSpace = 256;
+      colSpace = Math.round(screenWidth/cols); // space between columns depends on screen width
+      rows = Math.round(screenHeight/rowSpace);   // number of rows depends on screen height
 
-      //number of pizzas on the screen
+
+      //calculating the total number of pizzas on the screen
       numMovingPizzas = Math.ceil(cols * rows);
+      console.log("rows: " + rows + ", cols: " + cols + ", colSpace: " + colSpace + ", rowSpace: " + rowSpace + ", numMovingPizzas: " + numMovingPizzas);
 
   for (var i = 0; i < numMovingPizzas; i++) {
     var elem = document.createElement("img");
     elem.className = "mover";
     elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
-    elem.style.top = (Math.floor(i / cols) * vertSpace) + "px";
+    // elem.style.height = "100px";     // removing to set it in the stylesheet since this is statis number
+    // elem.style.width = "73.333px";   // removing to set it in the stylesheet since this is statis number
+
+    // this seems to calculate the initial distance between images
+    // 0.65 is a "magic number" that forces all image to display on the page, at least on the large screen
+    elem.basicLeft = (i % cols) * (colSpace * 0.65);
+    elem.style.top = (Math.floor(i / cols) * rowSpace) + "px"; // calculating the top property for each pizza based on row spacing
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
